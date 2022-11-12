c2b802f90fe0b0e6ee275bbb07500056e77f5642
Anton Logvinenko <anton.logvinenko@gmail.com>
2022-11-12 12:13:06 +0200

    Initial TUI setup

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
-use std::collections::{BTreeMap, HashMap};
+use crossterm::event::{self, DisableMouseCapture, EnableMouseCapture, Event as CEvent, KeyCode};
+use crossterm::terminal::{
+    disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
+};
+use std::collections::BTreeMap;
+use thiserror::Error;
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader, Lines};
+use std::sync::mpsc;
+use std::thread;
+use std::time::{Duration, Instant};
+use tui::layout::{Alignment, Constraint, Direction, Layout};
+use tui::style::{Color, Style};
+use tui::widgets::{Block, BorderType, Borders, Paragraph};
+use tui::{backend::CrosstermBackend, Terminal};
+
+//todo handling errors
+//todo project description
+//todo installation/compliation description
+
+
+enum Event<I> {
+    Input(I),
+    Tick,
+}
 
 fn lines(path: &str) -> io::Result<Lines<BufReader<File>>> {
     let file = File::open(path)?;
@@ -10,23 +32,22 @@ fn lines(path: &str) -> io::Result<Lines<BufReader<File>>> {
 
 fn add_to_vocabulary(vocab: &mut BTreeMap<String, Vec<String>>, str: &String) {
     //No, xml parsers make this code even worse
+    //Dealing with a single-tag constant-length xml wrapper here
     let x1 = str.split_at(92).1;
     let pos = x1.find("\">").expect("Unparseable line");
     let x2 = x1.split_at(pos);
     let key = x2.0;
     let value = String::from(&(x2.1)[2..(x2.1.len() - 10)]);
-
+    // println!("===============");
+    // println!("{}", html2text::from_read(value.as_bytes(), 100));
     //remove stress
     let chill = key.replace('\u{0301}', "");
-
     vocab.entry(chill).or_insert(Vec::new()).push(value);
 }
 
 fn get_vocabulary() -> BTreeMap<String, Vec<String>> {
     let vocab_path = "./bg-en.xml";
-
     let mut vocabulary: BTreeMap<String, Vec<String>> = BTreeMap::new();
-
     for line in lines(vocab_path).expect("Can't read vocabulary") {
         match line {
             Err(e) => {
@@ -41,11 +62,13 @@ fn get_vocabulary() -> BTreeMap<String, Vec<String>> {
     vocabulary.into_iter().skip(2287).collect()
 }
 
-fn main() -> io::Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let vocab = get_vocabulary();
 
     let batch: usize = 0;
     let batch_size = 100;
+
+    println!("Length {}", vocab.len());
 
     println!("Batches amount: {}", vocab.len() / batch_size);
 
@@ -55,7 +78,114 @@ fn main() -> io::Result<()> {
         .take(batch_size)
         .collect();
 
+    let (tx, rx) = mpsc::channel();
+    let tick_rate = Duration::from_millis(200);
+
+    thread::spawn(move || {
+        let mut last_tick = Instant::now();
+        loop {
+            let timeout = tick_rate
+                .checked_sub(last_tick.elapsed())
+                .unwrap_or_else(|| Duration::from_secs(0));
+
+            if event::poll(timeout).expect("poll works") {
+                if let CEvent::Key(key) = event::read().expect("can read events") {
+                    // println!(">>> Sending Event");
+                    tx.send(Event::Input(key)).expect("can send events");
+                }
+            }
+
+            if last_tick.elapsed() >= tick_rate {
+                if let Ok(_) = tx.send(Event::Tick) {
+                    // println!(">>> Sending OK");
+                    last_tick = Instant::now();
+                }
+            }
+        }
+    });
+
     println!("{}", batch_vocab.iter().count());
+
+    enable_raw_mode().expect("must be able to run in raw mode");
+    let mut stdout = io::stdout();
+    crossterm::execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.clear()?;
+
+    loop {
+        //todo add loop here
+        terminal.draw(|f| {
+            let copyright = Paragraph::new("pet-CLI 2020 - all rights reserved")
+                .style(Style::default().fg(Color::LightCyan))
+                .alignment(Alignment::Center)
+                .block(
+                    Block::default()
+                        .borders(Borders::ALL)
+                        .style(Style::default().fg(Color::White))
+                        .title("Copyright")
+                        .border_type(BorderType::Plain),
+                );
+            let size = f.size();
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(2)
+                .constraints(
+                    [
+                        Constraint::Length(3),
+                        Constraint::Min(2),
+                        Constraint::Length(3),
+                    ]
+                    .as_ref(),
+                )
+                .split(size);
+            f.render_widget(copyright, chunks[2]);
+
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(1)
+                .constraints(
+                    [
+                        Constraint::Percentage(10),
+                        Constraint::Percentage(80),
+                        Constraint::Percentage(10),
+                    ]
+                    .as_ref(),
+                )
+                .split(f.size());
+            let word = Block::default().title(" Word ").borders(Borders::ALL);
+            // f.render_widget(word, chunks[0]);
+            let translation = Block::default()
+                .title(" Translation ")
+                .borders(Borders::ALL);
+            // f.render_widget(translation, chunks[1]);
+        })?;
+
+        match rx.recv()? {
+            Event::Input(event) => match event.code {
+                KeyCode::Char('q') => {
+                    disable_raw_mode()?;
+                    terminal.show_cursor()?;
+                    break;
+                }
+                // KeyCode::Char('h') => active_menu_item = MenuItem::Home,
+                // KeyCode::Char('p') => active_menu_item = MenuItem::Pets,
+                _ => {}
+            },
+            Event::Tick => {}
+        }
+    }
+
+    // thread::sleep(Duration::from_millis(10000));
+
+    // restore terminal
+    disable_raw_mode()?;
+    // crossterm::execute!(
+    //     terminal.backend_mut(),
+    //     LeaveAlternateScreen,
+    //     DisableMouseCapture
+    // )?;
+    // terminal.show_cursor()?;
 
     Ok(())
 }
