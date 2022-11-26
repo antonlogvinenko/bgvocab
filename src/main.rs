465ab88ad2c42faab7013adbed477ea45c582afd
Anton Logvinenko <anton.logvinenko@gmail.com>
2022-11-26 13:07:37 +0200

    Displaying stress in words

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use clap::Parser;
-use crossterm::event::{self, DisableMouseCapture, EnableMouseCapture, Event as CEvent, KeyCode, KeyModifiers};
+use core::panic;
+use crossterm::event::{
+    self, DisableMouseCapture, EnableMouseCapture, Event as CEvent, KeyCode,
+};
 use crossterm::terminal::{
     disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
 };
-use core::panic;
 use std::collections::BTreeMap;
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader, Lines};
+use std::ops::Add;
 use std::sync::mpsc;
 use std::thread::{self};
 use std::time::{Duration, Instant};
@@ -21,7 +24,6 @@ use tui::{backend::CrosstermBackend, Terminal};
 //todo 3 not showing translation key
 //todo 2 show both words in translation
 //todo better layout/formatting
-
 
 //todo managing words
 
@@ -60,8 +62,6 @@ fn add_to_vocabulary(vocab: &mut BTreeMap<String, Vec<String>>, str: &String) {
     let x2 = x1.split_at(pos);
     let key = x2.0;
     let value = String::from(&(x2.1)[2..(x2.1.len() - 10)]);
-    // println!("===============");
-    // println!("{}", html2text::from_read(value.as_bytes(), 100));
     //remove stress
     let chill = key.replace('\u{0301}', "");
     vocab.entry(chill).or_insert(Vec::new()).push(value);
@@ -84,6 +84,43 @@ fn get_vocabulary() -> BTreeMap<String, Vec<String>> {
     vocabulary.into_iter().skip(2287).collect()
 }
 
+fn get_vocabulary2() -> BTreeMap<String, Vec<String>> {
+    let vocab_path = "./vocab.txt";
+    let mut vocabulary: BTreeMap<String, Vec<String>> = BTreeMap::new();
+    let mut x = lines(vocab_path).expect("must");
+
+    loop {
+        let maybe_word = x.next();
+        match maybe_word {
+            Some(Ok(mut word)) => {
+                let translation = x
+                    .next()
+                    .expect("translation must be present")
+                    .expect("must");
+
+                let indices: Vec<(usize, char)> = word.char_indices().collect();
+                let p = indices.iter().position(|(_, c)| c.is_uppercase());
+                match p {
+                    Some(pos) => {
+                        word = word.to_lowercase();
+                        let ins_at = if pos == indices.len() - 1 {
+                            word.len()
+                        } else {
+                            indices.get(pos + 1).unwrap().0
+                        };
+                        word.insert(ins_at, '\u{0301}');
+                    }
+                    None => {}
+                }
+
+                vocabulary.insert(word, vec![translation]);
+                x.next();
+            }
+            _ => return vocabulary,
+        }
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
 
@@ -91,9 +128,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         panic!("Batch size of less than 2 words? You make no sense.");
     }
 
-    let vocab = get_vocabulary();
+    // let vocab = get_vocabulary();
 
-    println!("Length {}", vocab.len());
+    let vocab = get_vocabulary2();
+
+    let vocab_size = vocab.len();
+    println!("Words in the dictionary: {:?}", vocab.len());
+
+    let mut x = String::from("aasd");
+    x.insert_str(1, "\u{0301}");
 
     println!("Batches amount: {}", vocab.len() / args.batch_size);
 
@@ -131,17 +174,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     });
 
-    println!("{}", batch_vocab.iter().count());
-
     enable_raw_mode().expect("must be able to run in raw mode");
     let mut stdout = io::stdout();
     crossterm::execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
+    // let mut terminal = Terminal::with_options(
+    //     backend,
+    //     TerminalOptions {
+    //         viewport: Viewport::fixed(Rect {x: 100, y: 0, width: 200, height:200})
+    //     },
+    // )?;
     terminal.clear()?;
 
     let mut index = 0;
-
+    //stretch indexation twice, /2 is index word, isEven is whether to show translation
+    //do +2 by default then add +1 via app key
     loop {
         let word = *keys.get(index).expect("must be in vocab");
         let translation: String = html2text::from_read(
@@ -162,43 +210,52 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .constraints(
                     [
                         Constraint::Percentage(10),
-                        Constraint::Percentage(80),
-                        Constraint::Percentage(10),
+                        Constraint::Percentage(70),
+                        Constraint::Percentage(20),
                     ]
                     .as_ref(),
                 )
                 .split(f.size());
 
-
-            let text = format!(" [{}/{}]    {}", index + 1, args.batch_size, word);
+            let text = format!(" [{}/{}] \n\n {}\n", index + 1, args.batch_size, word);
             let word_widget = Paragraph::new(Text::styled(
                 text,
                 Style::default()
                     .fg(Color::White)
-                    .add_modifier(Modifier::BOLD)
+                    .add_modifier(Modifier::BOLD),
             ))
             .style(Style::default())
             .block(
                 Block::default()
-                    .title(Span::styled("The word", Style::default().fg(Color::White)))
+                    .title(Span::styled("", Style::default().fg(Color::White)))
                     .borders(Borders::ALL),
             );
 
-
             f.render_widget(word_widget, chunks[0]);
 
-            let translation_widget = Paragraph::new(translation).style(Style::default()).block(
+            let translation_widget = Paragraph::new(Text::styled(
+                format!("\n {}", translation),
+                Style::default()
+                    .fg(Color::White)
+                    .add_modifier(Modifier::BOLD),
+            ))
+            .style(Style::default())
+            .block(
                 Block::default()
-                    .title(Span::styled(
-                        "The translation",
-                        Style::default().fg(Color::White),
-                    ))
+                    .title(Span::styled("", Style::default().fg(Color::White)))
                     .borders(Borders::ALL),
             );
             f.render_widget(translation_widget, chunks[1]);
 
+            let mut content: String = String::from(format!("\n Vocabulary size: {} words", vocab_size));
+            content = content
+                .add("\n")
+                .add("\n Press:")
+                .add("\n  <Enter> to see the next word")
+                .add("\n  <Backspace> to see the previous word")
+                .add("\n  q to exit");
             let help_widget = Paragraph::new(Text::styled(
-                "Press Enter for the next word; q to exit",
+                content,
                 Style::default()
                     // .fg(Color::Cyan)
                     .add_modifier(Modifier::BOLD),
@@ -224,13 +281,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     )?;
                     break;
                 }
-                KeyCode::Right | KeyCode::Enter => {
-                    if event.modifiers.contains(KeyModifiers::SHIFT) {
-                        index -= 1;
+                KeyCode::Backspace => {
+                    index = if index == 0 {
+                        keys.len() - 1
                     } else {
-                        index += 1;
-                    }
-                    index = index % keys.len();
+                        (index - 1) % keys.len()
+                    };
+                }
+                KeyCode::Enter => {
+                    index = (index + 1) % keys.len();
                 }
                 // KeyCode::Char('h') => active_menu_item = MenuItem::Home,
                 // KeyCode::Char('p') => active_menu_item = MenuItem::Pets,
