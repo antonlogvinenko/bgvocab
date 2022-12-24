8b1027d2f39f1209efc38e3ae4aea11953fd2b89
Anton Logvinenko <anton.logvinenko@gmail.com>
2022-12-24 16:15:03 +0200

    Moved common to lib module

--- vocab-bin/src/main.rs
+++ vocab-bin/src/main.rs
@@ -6,8 +6,7 @@ use crossterm::terminal::{
     disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
 };
 use std::collections::BTreeMap;
-use std::fs::File;
-use std::io::{self, prelude::*, BufReader, Lines, ErrorKind};
+use std::io::{self};
 use std::ops::Add;
 use std::sync::mpsc::{self, RecvError};
 use std::thread::{self};
@@ -23,7 +22,7 @@ use tui::{backend::CrosstermBackend, Terminal};
 pub enum VocabErrorX {
     #[error("data store disconnected")]
     Disconnect(#[from] io::Error),
-
+    
     #[error("data store disconnected")]
     Disconnect2(#[from] RecvError),
     // #[error("the data for key `{0}` is not available")]
@@ -57,82 +56,6 @@ enum Event<I> {
     Tick,
 }
 
-fn lines(path: &str) -> io::Result<Lines<BufReader<File>>> {
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
-    Ok(reader.lines())
-}
-
-fn add_to_vocabulary(vocab: &mut BTreeMap<String, Vec<String>>, str: &String) -> Result<(), VocabError> {
-    //No, xml parsers make this code even worse
-    //Dealing with a single-tag constant-length xml wrapper here
-    let x1 = str.split_at(92).1;
-    let pos = x1.find("\">").ok_or("Unparseable line")?;
-    let x2 = x1.split_at(pos);
-    let key = x2.0;
-    let value = String::from(&(x2.1)[2..(x2.1.len() - 10)]);
-    //remove stress
-    let chill = key.replace('\u{0301}', "");
-    vocab.entry(chill).or_insert(Vec::new()).push(value);
-    Ok(())
-}
-
-//https://nrc.github.io/error-docs/rust-errors/result-and-error.html
-type Vocab = BTreeMap<String, Vec<String>>;
-type VocabError = Box<dyn std::error::Error>;
-
-fn get_en_vocabulary() -> Result<Vocab, VocabError> {
-    let vocab_path = "./bg-en.xml";
-    let mut vocabulary: BTreeMap<String, Vec<String>> = BTreeMap::new();
-    for line in lines(vocab_path)? {
-        match line {
-            Err(e) => {
-                eprintln!("Failed to read line. {}", e);
-                return Err(std::io::Error::new(ErrorKind::NotFound, "Vocabulary could not be read"))?;
-            }
-            Ok(str) => add_to_vocabulary(&mut vocabulary, &str)?
-        }
-    }
-
-    //skip entries with names
-    Ok(vocabulary.into_iter().skip(2287).collect())
-}
-
-fn get_ru_vocabulary() -> Result<Vocab, VocabError> {
-    let vocab_path = "./vocab.txt";
-    let mut vocabulary: BTreeMap<String, Vec<String>> = BTreeMap::new();
-    let mut x = lines(vocab_path)?;
-
-    loop {
-        let maybe_word = x.next();
-        match maybe_word {
-            Some(Ok(mut word)) => {
-                let translation = x
-                    .next()
-                    .ok_or("translation must be present")??;
-
-                let indices: Vec<(usize, char)> = word.char_indices().collect();
-                let p = indices.iter().position(|(_, c)| c.is_uppercase());
-                match p {
-                    Some(pos) => {
-                        word = word.to_lowercase();
-                        let ins_at = if pos == indices.len() - 1 {
-                            word.len()
-                        } else {
-                            indices.get(pos + 1).unwrap().0
-                        };
-                        word.insert(ins_at, '\u{0301}');
-                    }
-                    None => {}
-                }
-
-                vocabulary.insert(word, vec![translation]);
-                x.next();
-            }
-            _ => return Ok(vocabulary),
-        }
-    }
-}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
@@ -142,9 +65,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     let vocab = if args.en {
-        get_en_vocabulary()
+        vocab_lib::get_en_vocabulary()
     } else {
-        get_ru_vocabulary()
+        vocab_lib::get_ru_vocabulary()
     }?;
 
     let vocab_size = vocab.len();
---
+++ vocab-lib/src/lib.rs
@@ -0,0 +1,79 @@
+use std::{collections::BTreeMap, io::{BufReader, Lines, self, BufRead, ErrorKind}, fs::File};
+
+pub type Vocab = BTreeMap<String, Vec<String>>;
+pub type VocabError = Box<dyn std::error::Error>;
+// type VocabError = VocabErrorX;
+
+pub fn lines(path: &str) -> io::Result<Lines<BufReader<File>>> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+    Ok(reader.lines())
+}
+
+pub fn add_to_vocabulary(vocab: &mut BTreeMap<String, Vec<String>>, str: &String) -> Result<(), VocabError> {
+    //No, xml parsers make this code even worse
+    //Dealing with a single-tag constant-length xml wrapper here
+    let x1 = str.split_at(92).1;
+    let pos = x1.find("\">").ok_or(std::io::Error::new(ErrorKind::NotFound, "Unparseable line"))?;
+    let x2 = x1.split_at(pos);
+    let key = x2.0;
+    let value = String::from(&(x2.1)[2..(x2.1.len() - 10)]);
+    //remove stress
+    let chill = key.replace('\u{0301}', "");
+    vocab.entry(chill).or_insert(Vec::new()).push(value);
+    Ok(())
+}
+
+
+pub fn get_en_vocabulary() -> Result<Vocab, VocabError> {
+    let vocab_path = "./bg-en.xml";
+    let mut vocabulary: BTreeMap<String, Vec<String>> = BTreeMap::new();
+    for line in lines(vocab_path)? {
+        match line {
+            Err(e) => {
+                eprintln!("Failed to read line. {}", e);
+                return Err(std::io::Error::new(ErrorKind::NotFound, "Vocabulary could not be read"))?;
+            }
+            Ok(str) => add_to_vocabulary(&mut vocabulary, &str)?
+        }
+    }
+
+    //skip entries with names
+    Ok(vocabulary.into_iter().skip(2287).collect())
+}
+
+pub fn get_ru_vocabulary() -> Result<Vocab, VocabError> {
+    let vocab_path = "./vocab.txt";
+    let mut vocabulary: BTreeMap<String, Vec<String>> = BTreeMap::new();
+    let mut x = lines(vocab_path)?;
+
+    loop {
+        let maybe_word = x.next();
+        match maybe_word {
+            Some(Ok(mut word)) => {
+                let translation = x
+                    .next()
+                    .ok_or(std::io::Error::new(ErrorKind::NotFound, "translation must be present"))??;
+
+                let indices: Vec<(usize, char)> = word.char_indices().collect();
+                let p = indices.iter().position(|(_, c)| c.is_uppercase());
+                match p {
+                    Some(pos) => {
+                        word = word.to_lowercase();
+                        let ins_at = if pos == indices.len() - 1 {
+                            word.len()
+                        } else {
+                            indices.get(pos + 1).unwrap().0
+                        };
+                        word.insert(ins_at, '\u{0301}');
+                    }
+                    None => {}
+                }
+
+                vocabulary.insert(word, vec![translation]);
+                x.next();
+            }
+            _ => return Ok(vocabulary),
+        }
+    }
+}
